7d034bfa00662a76d2c54bc0f1d34991f0b9c7af
Nerdo Pulido <nerdop@gmail.com>
2026-02-06 14:50:54 -0400

    FIX: OwlError unknown keys in ClosePosPopup props (Odoo 18 compatibility)

--- pos_show_dual_currency/static/src/js/Popups/ClosePosPopup.js
+++ pos_show_dual_currency/static/src/js/Popups/ClosePosPopup.js
@@ -1,30 +1,28 @@
-odoo.define('1010_pos_dual_currency.ClosePosPopup', function(require) {
+odoo.define('1010_pos_dual_currency.ClosePosPopup', function (require) {
     'use strict';
 
     const ClosePosPopup = require('point_of_sale.ClosePosPopup');
     const Registries = require('point_of_sale.Registries');
     const { identifyError } = require('point_of_sale.utils');
-    const { ConnectionLostError, ConnectionAbortedError} = require('@web/core/network/rpc_service')
+    const { ConnectionLostError, ConnectionAbortedError } = require('@web/core/network/rpc_service')
     const { useState } = owl;
 
-    const ClosePosPopupUSD = (ClosePosPopup) =>
-            class extends ClosePosPopup {
-                setup() {
-                    super.setup();
-                    this.manualInputCashCountUSD = false;
-                    Object.assign(this, this.props.info);
-                    this.state = useState({
-                        displayMoneyDetailsPopupUSD: false,
-                    });
+    const ClosePosPopupUSD = (ClosePosPopup) => {
+        class classUSD extends ClosePosPopup {
+            setup() {
+                super.setup();
+                this.manualInputCashCountUSD = false;
+                this.state = useState({
+                    displayMoneyDetailsPopupUSD: false,
+                    ...(this.props.state || {}),
+                });
+            }
 
-                    Object.assign(this.state, this.props.info.state);
-                    }
-
-                //@override
-                async confirm() {
-                    if (!this.cashControl || !this.hasDifferenceUSD()) {
-                        super.confirm();
-                    } else if (this.hasUserAuthorityUSD()) {
+            //@override
+            async confirm() {
+                if (!this.props.cashControl || !this.hasDifferenceUSD()) {
+                    super.confirm();
+                } else if (this.hasUserAuthorityUSD()) {
                     const { confirmed } = await this.showPopup('ConfirmPopup', {
                         title: this.env._t('Currency Ref Payments Difference'),
                         body: this.env._t('Do you want to accept currency ref payments difference and post a profit/loss journal entry?'),
@@ -32,90 +30,94 @@ odoo.define('1010_pos_dual_currency.ClosePosPopup', function(require) {
                     if (confirmed) {
                         super.confirm();
                     }
-                    } else {
+                } else {
                     await this.showPopup('ConfirmPopup', {
                         title: this.env._t('Currency Ref Payments Difference'),
                         body: _.str.sprintf(
                             this.env._t('The maximum difference by currency ref allowed is %s.\n\
                             Please contact your manager to accept the closing difference.'),
-                            this.env.pos.format_currency_ref(this.amountAuthorizedDiffUSD)
+                            this.env.pos.format_currency_ref(this.props.amount_authorized_diff_ref)
                         ),
                         confirmText: this.env._t('OK'),
                     })
                 }
+            }
+            openDetailsPopupUSD() {
+                this.state.payments_usd[this.props.default_cash_details.default_cash_details_ref.id].counted = 0;
+                this.state.payments_usd[this.props.default_cash_details.default_cash_details_ref.id].difference = -this.props.default_cash_details.default_cash_details_ref.amount;
+                this.state.displayMoneyDetailsPopupUSD = true;
+            }
+            closeDetailsPopupUSD() {
+                this.state.displayMoneyDetailsPopupUSD = false;
+            }
+
+            handleInputChangeUSD(paymentId) {
+                let expectedAmount;
+                if (paymentId === this.props.default_cash_details.default_cash_details_ref.id) {
+                    this.manualInputCashCountUSD = true;
+                    expectedAmount = this.props.default_cash_details.default_cash_details_ref.amount;
+                } else {
+                    expectedAmount = this.props.other_payment_methods.find(pm => paymentId === pm.id).amount;
                 }
-                openDetailsPopupUSD() {
+                this.state.payments_usd[paymentId].difference =
+                    this.env.pos.round_decimals_currency(this.state.payments_usd[paymentId].counted - expectedAmount);
+            }
 
-                    this.state.payments_usd[this.defaultCashDetails.default_cash_details_ref.id].counted = 0;
-                    this.state.payments_usd[this.defaultCashDetails.default_cash_details_ref.id].difference = -this.defaultCashDetails.default_cash_details_ref.amount;
-
-                    this.state.displayMoneyDetailsPopupUSD = true;
+            updateCountedCashUSD({ total_ref, moneyDetailsNotesRef }) {
+                const ref_id = this.props.default_cash_details.default_cash_details_ref.id;
+                this.state.payments_usd[ref_id].counted = total_ref;
+                this.state.payments_usd[ref_id].difference =
+                    this.env.pos.round_decimals_currency(this.state.payments_usd[ref_id].counted - this.props.default_cash_details.default_cash_details_ref.amount);
+                if (moneyDetailsNotesRef) {
+                    this.state.notes += moneyDetailsNotesRef;
                 }
-                closeDetailsPopupUSD() {
-                    this.state.displayMoneyDetailsPopupUSD = false;
-                 }
+                this.manualInputCashCountUSD = false;
+                this.closeDetailsPopupUSD();
+            }
 
-                handleInputChangeUSD(paymentId) {
+            hasDifferenceUSD() {
+                return Object.entries(this.state.payments_usd).find(pm => pm[1].difference != 0);
+            }
+            hasUserAuthorityUSD() {
+                const absDifferences = Object.entries(this.state.payments_usd).map(pm => Math.abs(pm[1].difference));
+                return this.props.is_manager || this.props.amount_authorized_diff_ref == null || Math.max(...absDifferences) <= this.props.amount_authorized_diff_ref;
+            }
 
-                    let expectedAmount;
-                    if (paymentId === this.defaultCashDetails.default_cash_details_ref.id) {
-                        this.manualInputCashCountUSD = true;
-                        expectedAmount = this.defaultCashDetails.default_cash_details_ref.amount;
-                    } else {
-                        expectedAmount = this.otherPaymentMethods.find(pm => paymentId === pm.id).amount;
-                    }
-                    this.state.payments_usd[paymentId].difference =
-                        this.env.pos.round_decimals_currency(this.state.payments_usd[paymentId].counted - expectedAmount);
-                }
-
-                updateCountedCashUSD({ total_ref, moneyDetailsNotesRef}) {
-                    this.state.payments_usd[this.defaultCashDetails.default_cash_details_ref.id].counted = total_ref;
-                    this.state.payments_usd[this.defaultCashDetails.default_cash_details_ref.id].difference =
-                        this.env.pos.round_decimals_currency(this.state.payments_usd[[this.defaultCashDetails.default_cash_details_ref.id]].counted - this.defaultCashDetails.default_cash_details_ref.amount);
-                    if (moneyDetailsNotesRef) {
-                        this.state.notes += moneyDetailsNotesRef;
-                    }
-                    this.manualInputCashCountUSD = false;
-                    this.closeDetailsPopupUSD();
-                }
-
-                hasDifferenceUSD() {
-                    return Object.entries(this.state.payments_usd).find(pm => pm[1].difference != 0);
-                }
-                hasUserAuthorityUSD() {
-                    const absDifferences = Object.entries(this.state.payments_usd).map(pm => Math.abs(pm[1].difference));
-                    return this.isManager || this.amountAuthorizedDiffUSD == null || Math.max(...absDifferences) <= this.amountAuthorizedDiffUSD;
-                }
-
-                async closeSession() {
-                    if (!this.closeSessionClicked) {
-                        this.closeSessionClicked = true;
-                        let response;
-                        if (this.cashControl) {
-                            response = await this.rpc({
-                                model: 'pos.session',
-                                method: 'post_closing_cash_details_ref',
-                                args: [this.env.pos.pos_session.id],
-                                kwargs: {
-                                counted_cash: this.state.payments_usd[this.defaultCashDetails.default_cash_details_ref.id].counted,
-                                }
-                            })
+            async closeSession() {
+                if (!this.closeSessionClicked) {
+                    this.closeSessionClicked = true;
+                    let response;
+                    if (this.props.cashControl) {
+                        response = await this.rpc({
+                            model: 'pos.session',
+                            method: 'post_closing_cash_details_ref',
+                            args: [this.env.pos.pos_session.id],
+                            kwargs: {
+                                counted_cash: this.state.payments_usd[this.props.default_cash_details.default_cash_details_ref.id].counted,
+                            }
+                        })
                         if (!response.successful) {
                             return super.handleClosingError(response);
                         }
-                        }
-                       await this.rpc({
-                            model: 'pos.session',
-                            method: 'update_closing_control_state_session_ref',
-                            args: [this.env.pos.pos_session.id, this.state.notes]
-                        })
-                        this.closeSessionClicked = false;
-                        }
-                    super.closeSession();
+                    }
+                    await this.rpc({
+                        model: 'pos.session',
+                        method: 'update_closing_control_state_session_ref',
+                        args: [this.env.pos.pos_session.id, this.state.notes]
+                    })
+                    this.closeSessionClicked = false;
                 }
-
-
-
+                super.closeSession();
+            }
+        }
+        classUSD.props = [
+            ...ClosePosPopup.props,
+            "other_payment_methods",
+            "amount_authorized_diff_ref",
+            "state",
+            "cashControl",
+        ];
+        return classUSD;
     };
 
     Registries.Component.extend(ClosePosPopup, ClosePosPopupUSD);
--- pos_show_dual_currency/static/src/js/Popups/MoneyDetailsPopup.js
+++ pos_show_dual_currency/static/src/js/Popups/MoneyDetailsPopup.js
@@ -1,4 +1,4 @@
-odoo.define('1010_pos_dual_currency.MoneyDetailsPopupUSD', function(require) {
+odoo.define('1010_pos_dual_currency.MoneyDetailsPopupUSD', function (require) {
     'use strict';
 
     const PosComponent = require('point_of_sale.PosComponent');
@@ -25,18 +25,18 @@ odoo.define('1010_pos_dual_currency.MoneyDetailsPopupUSD', function(require) {
         }
         get firstHalfMoneyDetailsRef() {
             const moneyDetailsKeysRef = Object.keys(this.state.moneyDetailsRef).sort((a, b) => a - b);
-            return moneyDetailsKeysRef.slice(0, Math.ceil(moneyDetailsKeysRef.length/2));
+            return moneyDetailsKeysRef.slice(0, Math.ceil(moneyDetailsKeysRef.length / 2));
         }
         get lastHalfMoneyDetailsRef() {
             const moneyDetailsKeysRef = Object.keys(this.state.moneyDetailsRef).sort((a, b) => a - b);
-            return moneyDetailsKeysRef.slice(Math.ceil(moneyDetailsKeysRef.length/2), moneyDetailsKeysRef.length);
+            return moneyDetailsKeysRef.slice(Math.ceil(moneyDetailsKeysRef.length / 2), moneyDetailsKeysRef.length);
         }
         updateMoneyDetailsAmountRef() {
             let total_ref = Object.entries(this.state.moneyDetailsRef).reduce((total_ref, money_ref) => total_ref + money_ref[0] * money_ref[1], 0);
             this.state.total_ref = this.env.pos.round_decimals_currency(total_ref);
         }
         confirm() {
-            let moneyDetailsNotesRef = this.state.total_ref  ? 'Ref Currency Money details: \n' : null;
+            let moneyDetailsNotesRef = this.state.total_ref ? 'Ref Currency Money details: \n' : null;
             this.env.pos.bills.forEach(bill => {
                 if (this.state.moneyDetailsRef[bill.value]) {
                     moneyDetailsNotesRef += `  - ${this.state.moneyDetailsRef[bill.value]} x ${this.env.pos.format_currency_ref(bill.value)}\n`;
@@ -54,6 +54,8 @@ odoo.define('1010_pos_dual_currency.MoneyDetailsPopupUSD', function(require) {
             this.props.onDiscard();
         }
     }
+
+    MoneyDetailsPopupUSD.props = ["manualInputCashCountUSD", "onConfirm", "onDiscard"];
 
     MoneyDetailsPopupUSD.template = 'MoneyDetailsPopupUSD';
     Registries.Component.add(MoneyDetailsPopupUSD);
