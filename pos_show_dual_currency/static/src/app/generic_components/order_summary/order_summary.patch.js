350ef637724fede156ffa5281e9989736a2b97e8
Nerdo Pulido <nerdop@gmail.com>
2026-02-08 09:22:06 -0400

    Fix POS crash: Moved total patch to OrderSummary

--- pos_show_dual_currency/static/src/app/generic_components/order_summary/order_summary.patch.js
+++ pos_show_dual_currency/static/src/app/generic_components/order_summary/order_summary.patch.js
@@ -1,10 +1,10 @@
 /** @odoo-module */
 
-import { OrderWidget } from "@point_of_sale/app/generic_components/order_widget/order_widget";
+import { OrderSummary } from "@point_of_sale/app/generic_components/order_summary/order_summary";
 import { patch } from "@web/core/utils/patch";
 import { useService } from "@web/core/utils/hooks";
 
-patch(OrderWidget.prototype, {
+patch(OrderSummary.prototype, {
     setup() {
         super.setup();
         this.pos = useService("pos");
@@ -14,7 +14,8 @@ patch(OrderWidget.prototype, {
         if (!this.pos || !this.pos.config.show_dual_currency) {
             return null;
         }
-        const total = this.props.taxTotals?.order_total || this.props.total || 0;
+        // OrderSummary receives 'total' or 'taxTotals' in props
+        const total = this.props.total || 0;
         return this.pos.format_currency_ref(total * this.pos.config.show_currency_rate);
     }
 });
