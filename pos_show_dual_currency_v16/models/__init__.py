22986c4d8e38f90ca409bea9d5171e3dbd935ad3
Nerdo Pulido <nerdop@gmail.com>
2026-02-09 14:53:27 -0400

    Cleanup: Remove legacy v16 POS folder

--- pos_show_dual_currency_v16/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import models
-from . import pos_session
-from . import pos_payment_method
-from . import pos_payment
-from . import pos_config
-from . import res_config_settings
-from . import pos_order
-from . import pos_order_line
-
--- pos_show_dual_currency_v16/models/res_config_settings.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models, _
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    pos_amount_authorized_diff_ref = fields.Float(related='pos_config_id.amount_authorized_diff_ref', readonly=False)
