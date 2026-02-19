3b5c1b98f4492058d367ff35b932223617f7e6f5
Nerdo Pulido <nerdop@gmail.com>
2026-02-19 13:23:45 -0400

    Fix: Add date_range module and remove redundant field definition

--- account_dual_currency/wizard/account_payment_register.py
+++ account_dual_currency/wizard/account_payment_register.py
@@ -32,7 +32,8 @@ class AccountPaymentRegister(models.TransientModel):
     amount_total_pagar = fields.Monetary(currency_field='currency_id', string="Total Pagar(Importe + IGTF):",
                                          readonly=True)
 
-    company_currency_id = fields.Many2one('res.currency', string='Company Currency')
+    # company_currency_id = fields.Many2one('res.currency', string='Company Currency')
+
 
     @api.depends('currency_id')
     def _get_default_igtf(self):
