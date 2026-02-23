5aecd5a186f878ca0689119a4e4e1944beaa9ab5
Nerdo Pulido <nerdop@gmail.com>
2026-02-23 08:01:38 -0400

    style: remove manual labels to allow native Odoo automatic field alignment (v18.0.1.0.31)

--- account_dual_currency/__manifest__.py
+++ account_dual_currency/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "[LocVe] Venezuela: Account Dual Currency",
-    'version': "18.0.1.0.16",
+    'version': "18.0.1.0.17",
     'category': 'LocVe [Localization]',
     'license': 'Other proprietary',
     'summary': """Esta aplicación permite manejar dualidad de moneda en Contabilidad.""",
--- l10n_ve_payment_extension/__manifest__.py
+++ l10n_ve_payment_extension/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Accountant/Accountant",
-    "version": "18.0.1.0.30",
+    "version": "18.0.1.0.31",
     # any module necessary for this one to work correctly
     "depends": [
         "base",
