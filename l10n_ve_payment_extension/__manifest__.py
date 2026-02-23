f5c6f8ac015ef7f24743095577afa0b38757cf8b
Nerdo Pulido <nerdop@gmail.com>
2026-02-22 23:18:58 -0400

    Refine Product UI anchors and fix labels (v18.0.1.0.22)

--- l10n_ve_payment_extension/__manifest__.py
+++ l10n_ve_payment_extension/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Accountant/Accountant",
-    "version": "18.0.1.0.21",
+    "version": "18.0.1.0.22",
     # any module necessary for this one to work correctly
     "depends": [
         "base",
