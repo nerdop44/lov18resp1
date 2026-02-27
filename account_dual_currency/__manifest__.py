f2561a36458177108444d4dbabc6de833db6def2
Nerdo Pulido <nerdop@gmail.com>
2026-02-27 12:11:43 -0400

    V8.21 - Limpieza V20: Tasa bajo Diario (campos estándar), Totales en rejilla 4 col (V21), Versión .101/.51

--- account_dual_currency/__manifest__.py
+++ account_dual_currency/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "[LocVe] Venezuela: Account Dual Currency",
-    'version': "18.0.1.0.100",
+    'version': "18.0.1.0.101",
     'category': 'LocVe [Localization]',
     'license': 'Other proprietary',
     'summary': """Esta aplicación permite manejar dualidad de moneda en Contabilidad.""",
