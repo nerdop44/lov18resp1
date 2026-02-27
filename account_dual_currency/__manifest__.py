d0419d88c8ec2151a1ef7f3406914ac891206f2f
Nerdo Pulido <nerdop@gmail.com>
2026-02-27 06:59:10 -0400

    V8.12 - Hard Force Sync: Version bump and XML dummy change for Odoo.sh detection

--- account_dual_currency/__manifest__.py
+++ account_dual_currency/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "[LocVe] Venezuela: Account Dual Currency",
-    'version': "18.0.1.0.91",
+    'version': "18.0.1.0.92",
     'category': 'LocVe [Localization]',
     'license': 'Other proprietary',
     'summary': """Esta aplicación permite manejar dualidad de moneda en Contabilidad.""",
