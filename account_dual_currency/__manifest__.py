af2c173b5d858f8e2d42ab5cf794b13a460dda04
Nerdo Pulido <nerdop@gmail.com>
2026-02-26 21:24:03 -0400

    V8.3 - Bump versions to force upgrade + side-by-side totals layout + fix report names

--- account_dual_currency/__manifest__.py
+++ account_dual_currency/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "[LocVe] Venezuela: Account Dual Currency",
-    'version': "18.0.1.0.80",
+    'version': "18.0.1.0.83",
     'category': 'LocVe [Localization]',
     'license': 'Other proprietary',
     'summary': """Esta aplicación permite manejar dualidad de moneda en Contabilidad.""",
