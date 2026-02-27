4a63f771674da69b1a40d985919c5f9144c3f9ea
Nerdo Pulido <nerdop@gmail.com>
2026-02-26 23:04:23 -0400

    V8.8 - Fix: hasclass() XPath warnings + Version bump for Green Build

--- l10n_ve_payment_extension/__manifest__.py
+++ l10n_ve_payment_extension/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Accountant/Accountant",
-    "version": "18.0.1.0.43",
+    "version": "18.0.1.0.44",
     # any module necessary for this one to work correctly
     "depends": [
         "base",
