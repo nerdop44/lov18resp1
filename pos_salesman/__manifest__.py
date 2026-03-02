1a4b8ca4c7fb5ce5d24c79e8020d9da1aff2b687
Nerdo Pulido <nerdop@gmail.com>
2026-03-02 12:17:43 -0400

    FIX: [ODOO18] XML attributes reverted to '1' and force_save removed from list views

--- pos_salesman/__manifest__.py
+++ pos_salesman/__manifest__.py
@@ -8,7 +8,7 @@
     'maintainer': 'José Luis Vizcaya López remake Ing Nerdo Pulido',
     'website': 'https://github.com/birkot',
     'category': 'Point of Sale',
-    "version": "18.0.1.0.8",
+    "version": "18.0.1.0.9",
     "application": False,
     'depends': ['base', 'point_of_sale', 'hr'],
     'data': [
