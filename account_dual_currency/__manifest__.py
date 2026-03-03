8ad53fa66d2d7fdac26faa506be2ad38e2010399
Nerdo Pulido <nerdop@gmail.com>
2026-03-03 16:05:22 -0400

    [LOCVE-21] HOTFIX: DB Schema update for inverse_rate

--- account_dual_currency/__manifest__.py
+++ account_dual_currency/__manifest__.py
@@ -1,6 +1,7 @@
 {
     'name': "[LOCVE] Venezuela: Account Dual Currency",
-    'version': "18.0.1.0.107",
+    'version': "18.0.1.0.108",
+
     'category': 'LocVe [Localization]',
     'license': 'Other proprietary',
     'summary': """Esta aplicación permite manejar dualidad de moneda en Contabilidad.""",
