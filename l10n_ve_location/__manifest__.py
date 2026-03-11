6017a991b790f77c821b31b2a46b2533a83511e0
Nerdo Pulido <nerdop@gmail.com>
2026-03-11 16:09:32 -0400

    chore: restoration of Binaural identity and version alignment (Rescue Operation)

--- l10n_ve_location/__manifest__.py
+++ l10n_ve_location/__manifest__.py
@@ -8,7 +8,7 @@ parroquias. 
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.13",
     "depends": ["base", "contacts"],
     "data": [
         "security/ir.model.access.csv",
