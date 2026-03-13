6abd0b58ac24a48d12318baf1fdc8d2d05d4aa5d
Nerdo Pulido <nerdop@gmail.com>
2026-03-13 13:52:55 -0400

    fix: remove openupgradelib dependency in date_range migration (V37)

--- date_range/migrations/18.0.4.0.0/post-migration.py
+++ date_range/migrations/18.0.4.0.0/post-migration.py
@@ -1,43 +1,32 @@
-from openupgradelib import openupgrade
+import logging
 
+_logger = logging.getLogger(__name__)
 
-@openupgrade.migrate()
-def migrate(env, version):
-    if openupgrade.table_exists(env.cr, "date_range_res_company_rel"):
-        openupgrade.logged_query(
-            env.cr,
-            """
+def migrate(cr, version):
+    if not version:
+        return
+
+    # Verificar si la tabla de la relación m2m existe antes de migrar
+    cr.execute("SELECT 1 FROM information_schema.tables WHERE table_name = 'date_range_res_company_rel'")
+    if cr.fetchone():
+        _logger.info("Migrating date_range company_id from many2many relation")
+        # Actualizar company_id desde la relación m2m si la columna company_id está vacía
+        cr.execute("""
             UPDATE date_range dr
             SET company_id = drrcr.res_company_id
             FROM date_range_res_company_rel drrcr,
-                date_range_type drt
+                 date_range_type drt
             WHERE
                 dr.id = drrcr.date_range_id
                 AND drt.id = dr.type_id
                 AND (drt.company_id IS NULL OR drt.company_id = drrcr.res_company_id)
-            """,
-        )
-        openupgrade.logged_query(
-            env.cr,
-            """SELECT
-                drrcr.res_company_id,
-                dr.id
-            FROM date_range dr
-            JOIN date_range_res_company_rel drrcr ON dr.id = drrcr.date_range_id
-            WHERE dr.company_id != drrcr.res_company_id
-            """,
-        )
-        for company_id, date_range_id in env.cr.fetchall():
-            date_range = env["date.range"].browse(date_range_id)
-            date_range_type = date_range.type_id
-            if (
-                date_range_type.company_id
-                and date_range_type.company_id.id != company_id
-            ):
-                date_range_type = date_range.type_id.copy({"company_id": company_id})
-            date_range.copy({"company_id": company_id, "type_id": date_range_type.id})
-    rule = env.ref("date_range.date_range_comp_rule")
+                AND dr.company_id IS NULL
+        """)
+
+    # Actualizar la regla de registro usando el Environment
+    from odoo import api, SUPERUSER_ID
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    
+    rule = env.ref("date_range.date_range_comp_rule", raise_if_not_found=False)
     if rule:
-        rule.domain_force = (
-            "['|',('company_id', 'in', company_ids),('company_id','=',False)]"
-        )
+        rule.domain_force = "['|',('company_id', 'in', company_ids),('company_id','=',False)]"
