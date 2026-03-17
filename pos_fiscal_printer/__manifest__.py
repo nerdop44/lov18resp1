c6be4ffccd9631652b2418a80d563076388df45e
Nerdo Pulido <nerdop@gmail.com>
2026-03-17 18:06:52 -0400

    v18.0.1.1.15 - Hotfix: Inyección de checkFiscalStatus en ReceiptScreen. Pachacutec.

--- pos_fiscal_printer/__manifest__.py
+++ pos_fiscal_printer/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': '[LOCVE] Venezuela: POS fiscal printer',
-    "version": "18.0.1.1.14",
+    "version": "18.0.1.1.15",
     'category': 'LocVe [Localization]',
     'summary': 'Fiscal printing using serial ports',
     'author': 'Remake Ing Nerdo Pulido',
